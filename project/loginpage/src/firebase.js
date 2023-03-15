eb2399abfd8839a60a9c1907a64e94870a776070
karanA007 <105054970+karanA007@users.noreply.github.com>
2023-03-15 13:52:17 +0530

    Update firebase.js

--- project/loginpage/src/firebase.js
+++ project/loginpage/src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: <YOURAPIKEY>,
-  authDomain: <YOURAUTHDOAMIN>,
-  projectId: <YOURPROJECTID>,
-  storageBucket: <YOURSTORAGEBUCKET>,
-  messagingSenderId: <YOURMESSAGINGSENDERID>,
-  appId: <YOURAPPID>,
+  apiKey: "AIzaSyBflOt95-GfuGLGfKn7303ltl9DAu7wOzw",
+  authDomain: "auth-dev-a285f.firebaseapp.com",
+  projectId: "auth-dev-a285f",
+  storageBucket: "auth-dev-a285f.appspot.com",
+  messagingSenderId: "554456809698",
+  appId: "1:554456809698:web:4c728c41a0ea92a95d5ebb",
+  measurementId: "G-QTM0SSP5KW"
 };
 
 // Initialize Firebase
